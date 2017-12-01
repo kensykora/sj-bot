d3071f749504c022d80900863ef4b89d3d8ccefe
Ken Sykora <ken.sykora@gmail.com>
2017-11-30 23:47:51 -0600

    basic webapp with keep alive

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 var logger = require('winston');
-//var auth = optional('./auth.json');
+const express = require('express');
+const app = express();
 
 const idiotRoleName = 'idiots'
 const idiotGameName = 'World of Warcraft'
@@ -84,3 +85,29 @@ bot.on('presenceUpdate', (oldMember, newMember) => {
 });
 
 bot.login(process.env.DISCORD_KEY);
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+    // ejs render automatically looks in the views folder
+    response.render('index');
+});
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+ // pings server every 15 minutes to prevent dynos from sleeping
+ setInterval(() => {
+    http.get('http://morning-stream-21025.herokuapp.com');
+  }, 900000);
